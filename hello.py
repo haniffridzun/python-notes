7c61cc0a7b42c4c0537de6c6f90b9a8b998ce197
haniffridzun <haniffridzun@gmail.com>
2021-06-12 20:11:27 +0800

    First hello from python

---
+++ hello.py
@@ -0,0 +1,12 @@
+# this program says hello and asks for my name.
+print('hello world!')
+print('what is your name?')
+# save input into variable myName
+myName = input()
+print('it is good to mee tyou, ' + myName)
+print('the length of your name is:')
+print(len(myName))
+print('what is your age?')
+# save input into variable myAge
+myAge = input()
+print('you will be ' + str(int(myAge) + 1) + ' in a year.')
