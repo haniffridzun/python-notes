3d8379b312e78f11219d356627b9658889c6dd43
haniffridzun <haniffridzun@gmail.com>
2021-06-12 21:53:48 +0800

    Intro to python

--- hello.py
+++ hello.py
@@ -5,8 +5,10 @@ print('what is your name?')
 myName = input()
 print('it is good to mee tyou, ' + myName)
 print('the length of your name is:')
+# len() function - evaluates number of character in that string
 print(len(myName))
 print('what is your age?')
 # save input into variable myAge
-myAge = input()
+myAge = input() # input() function always returns a string, even if user enters number
+# str() function - concatenate int with string
 print('you will be ' + str(int(myAge) + 1) + ' in a year.')
